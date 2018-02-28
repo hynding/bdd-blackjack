faf2fd9b32857aa7543490c6bd15f798991c30eb
hynding <steve.hynding@bcgdv.com>
2018-02-28 09:01:27 -0800

    fill steps

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,20 +1,27 @@
 const { defineSupportCode } = require('cucumber')
+const { Deck } = require('../../index')
+const { equals } = require('assert')
 
 defineSupportCode(function ({ Given, When, Then }) {
 
-  Given('I have a new deck of cards', function () {
+  let deck, hand
 
+  Given('I have a new deck of cards', function () {
+    deck = new Deck()
   })
 
   When('I deal all cards', function () {
-
+    hand = []
+    while (deck.remaining) {
+      hand.push(deck.deal())
+    }
   })
 
   Then('I should have a hand of 52 cards', function () {
-
+    equals(hand.length, 52)
   })
 
   Then('I should have no more cards in the deck', function () {
-
+    equals(deck.remaining, 0)
   })
 })
