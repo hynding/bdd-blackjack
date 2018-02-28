db8abe8a0fe358da25b5743c99588f74dbbcc104
hynding <steve.hynding@bcgdv.com>
2018-02-28 12:19:21 -0800

    refactored steps

--- features/playing-cards.feature
+++ features/playing-cards.feature
@@ -2,62 +2,24 @@ Feature: A Standard Deck of Playing Cards
 
   Scenario: Inspecting a New Deck
     Given I have a new deck of cards
+    And the face order of each suit is
+      | Ace   |
+      | Two   |
+      | Three |
+      | Four  |
+      | Five  |
+      | Six   |
+      | Seven |
+      | Eight |
+      | Nine  |
+      | Ten   |
+      | Jack  |
+      | Queen |
+      | King  |
     When I deal all cards
     Then I should have a hand of 52 cards
     And I should have no more cards in the deck
-    And the first set of 13 cards should be Hearts in the following order
-      | Ace   |
-      | Two   |
-      | Three |
-      | Four  |
-      | Five  |
-      | Six   |
-      | Seven |
-      | Eight |
-      | Nine  |
-      | Ten   |
-      | Jack  |
-      | Queen |
-      | King  |
-    And the second set of 13 cards should be Clubs in the following order
-      | Ace   |
-      | Two   |
-      | Three |
-      | Four  |
-      | Five  |
-      | Six   |
-      | Seven |
-      | Eight |
-      | Nine  |
-      | Ten   |
-      | Jack  |
-      | Queen |
-      | King  |
-    And the third set of 13 cards should be Diamonds in the following order
-      | Ace   |
-      | Two   |
-      | Three |
-      | Four  |
-      | Five  |
-      | Six   |
-      | Seven |
-      | Eight |
-      | Nine  |
-      | Ten   |
-      | Jack  |
-      | Queen |
-      | King  |
-    And the fourth set of 13 cards should be Spades in the following order
-      | Ace   |
-      | Two   |
-      | Three |
-      | Four  |
-      | Five  |
-      | Six   |
-      | Seven |
-      | Eight |
-      | Nine  |
-      | Ten   |
-      | Jack  |
-      | Queen |
-      | King  |
+    And the 1st set of 13 cards in my hand should be "Hearts" in face order
+    And the 2nd set of 13 cards in my hand should be "Clubs" in face order
+    And the 3rd set of 13 cards in my hand should be "Diamonds" in face order
+    And the 4th set of 13 cards in my hand should be "Spades" in face order
--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,75 +1,39 @@
-const { defineSupportCode } = require('cucumber')
+const { Given, When, Then } = require('cucumber')
 const { Deck } = require('../../index')
 const { equal, ok } = require('assert')
 
-defineSupportCode(function ({ Given, When, Then }) {
+let deck, hand, faceOrder
 
-  let deck, hand
+Given('I have a new deck of cards', function () {
+  deck = new Deck()
+})
 
-  Given('I have a new deck of cards', function () {
-    deck = new Deck()
-  })
+Given('the face order of each suit is', function(data) {
+  faceOrder = data.raw().map(faceName => faceName[0])
+})
 
-  When('I deal all cards', function () {
-    hand = []
-    while (deck.remaining) {
-      hand.push(deck.deal())
-    }
-  })
+When('I deal all cards', function () {
+  hand = []
+  while (deck.remaining) {
+    hand.push(deck.deal())
+  }
+})
 
-  Then('I should have a hand of 52 cards', function () {
-    equal(hand.length, 52)
-  })
+Then('I should have a hand of 52 cards', function () {
+  equal(hand.length, 52)
+})
 
-  Then('I should have no more cards in the deck', function () {
-    equal(deck.remaining, 0)
-  })
+Then('I should have no more cards in the deck', function () {
+  equal(deck.remaining, 0)
+})
 
-  Then('the first set of 13 cards should be Hearts in the following order', function (order) {
-    const cardSet = hand.slice(0, 13)
-    const firstCard = cardSet[0]
-    const lastCard = cardSet[cardSet.length - 1]
-    const cardFaceOrder = order.raw()
-    cardSet.forEach((card, index) => {
-      const cardFaceName = cardFaceOrder[index][0]
-      equal(Deck.suits[card.suit], 'Hearts')
-      equal(Deck.faces[card.face], cardFaceName)
-    })
-  })
-
-  Then('the second set of 13 cards should be Clubs in the following order', function (order) {
-    const cardSet = hand.slice(13, 26)
-    const firstCard = cardSet[0]
-    const lastCard = cardSet[cardSet.length - 1]
-    const cardFaceOrder = order.raw()
-    cardSet.forEach((card, index) => {
-      const cardFaceName = cardFaceOrder[index][0]
-      equal(Deck.suits[card.suit], 'Clubs')
-      equal(Deck.faces[card.face], cardFaceName)
-    })
-  })
-
-  Then('the third set of 13 cards should be Diamonds in the following order', function (order) {
-    const cardSet = hand.slice(26, 39)
-    const firstCard = cardSet[0]
-    const lastCard = cardSet[cardSet.length - 1]
-    const cardFaceOrder = order.raw()
-    cardSet.forEach((card, index) => {
-      const cardFaceName = cardFaceOrder[index][0]
-      equal(Deck.suits[card.suit], 'Diamonds')
-      equal(Deck.faces[card.face], cardFaceName)
-    })
-  })
-
-  Then('the fourth set of 13 cards should be Spades in the following order', function (order) {
-    const cardSet = hand.slice(39)
-    const firstCard = cardSet[0]
-    const lastCard = cardSet[cardSet.length - 1]
-    const cardFaceOrder = order.raw()
-    cardSet.forEach((card, index) => {
-      const cardFaceName = cardFaceOrder[index][0]
-      equal(Deck.suits[card.suit], 'Spades')
-      equal(Deck.faces[card.face], cardFaceName)
-    })
+Then('the {word} set of {int} cards in my hand should be {string} in face order', function (place, count, suit) {
+  const placeIndex = Number(place.slice(0, -2)) - 1
+  const setIndex = placeIndex * count
+  const cardSet = hand.slice(setIndex, setIndex + count)
+  cardSet.forEach((card, index) => {
+    const cardFaceName = faceOrder[index]
+    equal(Deck.suits[card.suit], suit)
+    equal(Deck.faces[card.face], cardFaceName)
   })
 })
